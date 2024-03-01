7c515d781790b4b8ecfccfb1cdc64fc4b905d41a
laihaoqiang <laihaoqiang34@gmail.com>
2024-03-02 03:57:21 +1000

    Text detetion from Image

---
+++ 1.py
@@ -0,0 +1,28 @@
+from PIL import Image
+import pytesseract
+# import pandas as pd
+import json
+
+img = Image.open('1.jpg') 
+text = pytesseract.image_to_string(img)
+# print(type(text))
+# f = open('file.txt', 'w')
+# f.writelines(text)
+txt= text.replace("\n", "")
+numOfDate = txt.find('Preferred start date')
+numOfAccountNum = txt.find('(user number)')
+# print(numOfDate)
+# print(numOfAccountNum)
+useraccount_number = txt[numOfAccountNum+13: numOfAccountNum+28]
+# print(useraccount_number)
+preferred_start_date = txt[numOfDate+20: numOfDate+30]
+# print(preferred_start_date)
+data = {
+    "useraccout_number": useraccount_number,
+    "preferred_start_date": preferred_start_date
+}
+
+with open('data.json', 'w') as f:
+    json.dump(data, f)
+f = open('file.txt', 'w')
+f.writelines(txt)
